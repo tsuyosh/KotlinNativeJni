e5c26d1e33babd4ef2c27dd6c84f2950aa0b10d7
Tsuyoshi Uehara <tsuyosh@linecorp.com>
2019-01-23 15:58:28 +0900

    Change function name of entry point of JNI

--- greeting/src/armMain/kotlin/io/github/tsuyosh/kotlinnativejni/GreetingJni.kt
+++ greeting/src/armMain/kotlin/io/github/tsuyosh/kotlinnativejni/GreetingJni.kt
@@ -9,7 +9,7 @@ import platform.android.*
  */
 // for usage of @CName, see https://github.com/JetBrains/kotlin-native/blob/master/backend.native/tests/produce_dynamic/simple/hello.kt#L31
 @CName("Java_io_github_tsuyosh_kotlinnativejni_Greeting_say")
-fun Java_io_github_tsuyosh_kotlinnativejni_Greeting_say(
+fun Jni_Greeting_say(
     env: CPointer<JNIEnvVar>?,
     obj: jobject?,
     name: jstring?
