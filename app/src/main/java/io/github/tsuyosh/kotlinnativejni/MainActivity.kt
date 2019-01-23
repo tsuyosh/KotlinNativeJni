815d14570fc084f0bc75c0c277abfa88aea330cd
Tsuyoshi Uehara <tsuyosh@linecorp.com>
2019-01-23 15:59:17 +0900

    Change name to "World" and enlarge text side for demonstration

--- app/src/main/java/io/github/tsuyosh/kotlinnativejni/MainActivity.kt
+++ app/src/main/java/io/github/tsuyosh/kotlinnativejni/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.greeting_textview).text = Greeting().say("ほげほげ")
+        findViewById<TextView>(R.id.greeting_textview).text = Greeting().say("World")
     }
 }
